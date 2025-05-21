6cd7d54c98e0a660d4f5feb422be11f77aac4788
Manoj Mamidyala <manojmamidyala36@gmail.com>
2025-05-21 09:51:51 +0530

    init

---
+++ src/main/java/org/example/BatchingTimeoutTest.java
@@ -0,0 +1,66 @@
+package org.example;
+
+import org.apache.pulsar.client.api.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class BatchingTimeoutTest {
+
+    public static void main(String[] args) throws Exception {
+
+        int producerCnt = 50;
+        List<CompletableFuture<Void>> futureList = new ArrayList<>(producerCnt);
+        final PulsarClient client = PulsarClient.builder()
+                .serviceUrl("pulsar://localhost:6650")
+                .build();
+        final Executor testExecutor = Executors.newFixedThreadPool(50);
+        for (int i = 0; i < producerCnt; i++) {
+            final int j = i;
+            CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+                BatchingTimeoutTest batchingTimeoutTest = new BatchingTimeoutTest();
+                batchingTimeoutTest.produce(client, "producer" + j);
+            }, testExecutor);
+            futureList.add(future);
+            Thread.sleep(1000);
+        }
+
+        futureList.forEach(CompletableFuture::join);
+        client.close();
+    }
+
+    public void produce(PulsarClient client, String producerName) {
+        try {
+            int numMessages = 20;
+            Producer<byte[]> producer = client.newProducer()
+                    .topic("persistent://public/default/test-batching-timeout")
+                    .enableBatching(true)
+                    .batchingMaxPublishDelay(1, TimeUnit.MILLISECONDS) // aggressive batching
+                    .sendTimeout(5, TimeUnit.SECONDS)                  // short timeout
+                    .blockIfQueueFull(true)
+                    .producerName(producerName)
+                    .create();
+
+            List<CompletableFuture<MessageId>> futureList = new ArrayList<>(numMessages);
+            for (int i = 0; i < numMessages; i++) {
+                try {
+                    futureList.add(producer.sendAsync(("Message " + i).getBytes()));
+                    System.out.println("producer " + producerName + " Queued message " + i);
+                    Thread.sleep(5000); // > sendTimeout to trigger the bug
+                } catch (Exception e) {
+                    System.err.println("Failed producer " + producerName + " to send message " + i + ": " + e.getMessage());
+                }
+            }
+
+            futureList.forEach(CompletableFuture::join);
+            producer.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
